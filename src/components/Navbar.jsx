9cfb6ec23d57c4d1ef03c1ec30630109ffc4f9fa
Anton dela Cruz <adcruzer02@gmail.com>
2026-03-12 14:55:15 -0400

    react intro
    created components, styling, and webpages for...
    - Card
    - Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                <h2>My App</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+
+export default Navbar;
